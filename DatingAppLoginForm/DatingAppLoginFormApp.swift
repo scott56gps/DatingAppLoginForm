afffc41c7b97148de4cffbd0f4e64f788f4e380f
Scott Nicholes <scott93gps@gmail.com>
2026-01-01 01:42:57 -0800

    Initial Commit

---
+++ DatingAppLoginForm/DatingAppLoginFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingAppLoginFormApp.swift
+//  DatingAppLoginForm
+//
+//  Created by Scott Nicholes on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct DatingAppLoginFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
